3bf4380ec75f2553afdd4160341f37dd70fced70
nemantro <slim@slimsnippets.com>
2018-08-10 17:45:45 +0200

    Console App with json config

---
+++ SolutionBoilerPlate/ConsoleBoilerPlate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleBoilerPlate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional:true, reloadOnChange:true);
+
+            var configuration = builder.Build();
+
+            var UseConsoleColors = configuration.GetValue("UseConsoleColors", false);
+
+            Console.WriteLine($"UseConsoleColors {UseConsoleColors}");
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}

935f200c76f0f16c151b2f81d4f858a08cd43a8e
nemantro <slim@slimsnippets.com>
2018-09-04 19:10:13 +0200

    Add dependency injection

--- SolutionBoilerPlate/ConsoleBoilerPlate/Program.cs
+++ SolutionBoilerPlate/ConsoleBoilerPlate/Program.cs
@@ -1,29 +1,48 @@
 ﻿using System;
 using System.IO;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ConsoleBoilerPlate
 {
     class Program
     {
+        private static IConsoleLogger _consoleLogger;
+        private static IConfigurationRoot _configuration;
+
         static void Main(string[] args)
+        {
+            InitializeServices();
+
+            _consoleLogger.LogDebug("Debug line 8-)");
+            _consoleLogger.LogInformation("Information line :D");
+            _consoleLogger.LogError("Error line :'( ");
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }        
+
+        private static void InitializeServices()
         {
             var builder = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional:true, reloadOnChange:true);
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
-            var configuration = builder.Build();
+            _configuration = builder.Build();
 
-            var UseConsoleColors = configuration.GetValue("UseConsoleColors", false);
+            //Get Configuration
+            var UseConsoleColors = _configuration.GetValue("UseConsoleColors", false);
 
-            ConsoleLogger consoleLogger = new ConsoleLogger(UseConsoleColors);
+            //Configure injection
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IConsoleLogger>(
+                provider => new ConsoleLogger(UseConsoleColors)
+            );
 
-            consoleLogger.LogDebug("Debug line 8-)");
-            consoleLogger.LogInformation("Information line :D");
-            consoleLogger.LogError("Error line :'( ");
-            
-            Console.WriteLine("Press enter to exit");            
-            Console.ReadLine();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            //Inject Services
+            _consoleLogger = serviceProvider.GetService<IConsoleLogger>();
         }
     }
 }

4adb619f878ee51ed7abaabe951d0c17108c7bd5
Slim Ayache <slim.ayache@younited-credit.fr>
2018-10-17 13:10:21 +0200

    testing project - first commit

---
+++ slimsnippets.testing/slimsnippets.testing.console/LogLevel.cs
@@ -0,0 +1,6 @@
+internal enum LogLevel
+{
+    Debug,
+    Information,
+    Error,
+}
---
+++ slimsnippets.testing/slimsnippets.testing.console/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleBoilerPlate
+{
+    class Program
+    {
+        private static IConsoleLogger _consoleLogger;
+        private static IConfigurationRoot _configuration;
+
+        static void Main(string[] args)
+        {
+            InitializeServices();
+
+            _consoleLogger.LogDebug("Debug line 8-)");
+            _consoleLogger.LogInformation("Information line :D");
+            _consoleLogger.LogError("Error line :'( ");
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }        
+
+        private static void InitializeServices()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            _configuration = builder.Build();
+
+            //Get Configuration
+            var UseConsoleColors = _configuration.GetValue("UseConsoleColors", false);
+
+            //Configure injection
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IConsoleLogger>(
+                provider => new ConsoleLogger(UseConsoleColors)
+            );
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            //Inject Services
+            _consoleLogger = serviceProvider.GetService<IConsoleLogger>();
+        }
+    }
+}
